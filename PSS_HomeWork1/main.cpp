abaeef40eeb8a71db8405301876c31db6bf6a2e2
V_Roman_V <vor.roman.v@gmail.com>
2021-02-04 11:26:24 +0300

    now the case of the word is not important, added a little optimization

--- PSS_HomeWork1/main.cpp
+++ PSS_HomeWork1/main.cpp
@@ -3,67 +3,78 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <set>
 #include <unordered_map>
 #include <algorithm>
 using namespace std;
 
 // есть набор предложений они лежат в двумерном массиве (слов)
-// есть структура (может быть хэшмап) хранящая слова для каждого слова есть массив, где оно встретилось
-class Word{
+// есть структура хранящая локации для каждого слова встретившегося в массиве
+
+class WordLoc{
 public:
     vector<pair<unsigned int,unsigned int> > location; // sentence + number_in_the_sentence
 };
 
+inline void toUpper(const string& wordIn, string& wordOut){
+    transform(wordIn.begin(), wordIn.end(),wordOut.begin(), ::toupper);
+}
+
+inline void toLower(const string& wordIn, string& wordOut){
+    transform(wordIn.begin(), wordIn.end(),wordOut.begin(), ::tolower);
+}
+
 int main()
 {
     vector< vector<string> > text(1);
-    unordered_map<string, Word> dictionary;
+    unordered_map<string, WordLoc> dictionary;
     ifstream file;
     file.open("C:/Works/PSS_HomeWork/PSS_HomeWork1/in.txt");
     string text_in;
     getline(file, text_in);
     stringstream in(text_in);
-    string word;
+    string wordInText;
     unsigned int countS = 0;
     unsigned int countW = 0;
-    while( in >> word){
+    while( in >> wordInText){
         bool newSent = false;
-        if(word.back() == '.'){
-            word.pop_back();
+        if(wordInText.back() == '.'){
+            wordInText.pop_back();
             newSent = true;
             text.push_back(vector<string>());
         }
-        text[countS].push_back(word);
-        auto pair = dictionary.emplace(word,Word());
-        auto iter = pair.first;
-        iter->second.location.push_back(make_pair(countS,countW));
+        text[countS].push_back(wordInText);
+
+        toLower(wordInText,wordInText);
+        //adds its location to the word
+        dictionary.emplace(wordInText,WordLoc()).first->second.location.push_back(make_pair(countS,countW));
+
         countS += newSent;
         countW  = newSent?0:(countW+1);
     }
+
     int cWords;
     file >> cWords; // this action is only needed to remove the number at the beginning
-
-    while(file >> word){
-        vector < set <unsigned int> > needUppercase(text.size());
-        auto iter = dictionary.find(word);
+    string wordToFind;
+    while(file >> wordToFind){
+        toLower(wordToFind, wordToFind);
+        auto iter = dictionary.find(wordToFind);
         if(iter == dictionary.end()){
-            cout<<word<<"There is no such word in the text"<<endl;
+            cout<<0<<endl<<wordToFind<<": There is no such word in the text"<<endl;
             continue;
         }
-        auto location = iter->second.location; // vector< uint, uint>
-        for( auto loc:location)
-            needUppercase[loc.first].insert(loc.second);
+
+        const auto& location = iter->second.location; // vector< pair< uint, uint > >
+        auto locIt = location.begin();// iterator to the beginning of the location array
 
         cout<<location.size()<<endl;
-        for(unsigned int i=0; i<needUppercase.size() ;i++){
-            if(needUppercase[i].empty())continue;
-            auto up = needUppercase[i].begin();
+        for(unsigned int i=0; i<text.size() ;i++){
+            if(locIt==location.end())break;
+            if(i < locIt->first)continue;
             for(unsigned int j=0;j<text[i].size();j++){
                 string word = text[i][j];
-                if(j == *up){
-                    up++;
-                    transform(word.begin(), word.end(),word.begin(), ::toupper);
+                if(locIt!=location.end() && i == locIt->first && j == locIt->second){
+                    locIt++;
+                    toUpper(word, word);
                 }
                 cout<<word<<((j+1<text[i].size())?" ":"");
             }
@@ -71,25 +82,5 @@ int main()
         }
         cout<<endl;
     }
-
-    // Print for debug
-    /*for(auto sentence: text){
-        for(auto word: sentence)
-            cout<<word<<" ";
-        cout<<endl;
-    }
-    for(auto word: dictionary){
-        cout<<word.first<<": ";
-        for(auto l: word.second.location)
-            cout<<l.first<<"-"<<l.second<<" ";
-        cout<<";"<<endl;
-    }
-    cout<<endl;
-    for(unsigned int i=0 ;i < needUppercase.size();i++){
-        cout<<i<<": ";
-        for(auto w: needUppercase[i])
-            cout<<w;
-        cout<<";"<<endl;
-    }*/
     return 0;
 }
